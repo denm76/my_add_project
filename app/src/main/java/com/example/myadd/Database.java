35c8d39263f78a6edc4557e143eb5ef06c407a58
denm76 <denm76@gmail.com>
2023-06-24 11:33:59 +0300

    Create a Database class

---
+++ app/src/main/java/com/example/myadd/Database.java
@@ -0,0 +1,45 @@
+package com.example.myadd;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Database {
+
+    private ArrayList<Add> adds = new ArrayList<>();
+
+    //паттерн Синглтон для создания объекта класса(БД), доступного из разных классов
+    private static Database instance = null;
+
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+    // end pattern
+
+    private Database() {
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            Add add = new Add("Add" + i, "Add" + i, random.nextInt(1500) + 20, random.nextInt(3));
+            adds.add(add);
+        }
+    }
+
+    public void add(Add add) {
+        adds.add(add);
+    }
+
+    public void remove(int id) {
+        for (int i = 0; i < adds.size(); i++) {
+            Add add = adds.get(i);
+            if (add.getId() == id) {
+                adds.remove(add);
+            }
+        }
+    }
+
+    public ArrayList<Add> getNotes() {
+        return new ArrayList<>(adds);
+    }
+}
